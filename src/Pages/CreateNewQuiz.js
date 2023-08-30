5779550af62efdeed9a90ad8e34b1cd1be8a36ce
nidhisahani <nidhinishad42@gmail.com>
2023-08-30 16:20:57 -0700

    create new Quiz

---
+++ src/Pages/CreateNewQuiz.js
@@ -0,0 +1,16 @@
+import { Alert } from '@mui/material'
+import React from 'react'
+import AlertOpt from '../components/AlertOpt'
+
+function CreateNewQuiz() {
+  return (
+    <>
+      <Alert>
+        <AlertOpt />
+      </Alert>
+      
+    </>
+  )
+}
+
+export default CreateNewQuiz
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,7 +9,7 @@ const HeroSection = () => {
     return (
         <Grid container sx={6} style={{ width: '100%', marginTop: '10%',marginLeft:'10%'}}>
            
-           <Grid item sx={4} >
+           <Grid  >
                    
                 <CardActionArea >                        
                     <CardContent>  
@@ -18,8 +18,10 @@ const HeroSection = () => {
                     </CardContent>                         
                 </CardActionArea>
             </Grid> 
+
+            <br />
            
-            <Grid item xs={4} width='150px' >
+            <Grid  >
               
               <CardActionArea >                        
                   <CardContent> 
